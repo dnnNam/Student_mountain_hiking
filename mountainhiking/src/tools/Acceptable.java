c943e8aafba39e56df38569e4d0b6cbd999e7314
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-02-11 15:07:17 +0700

    update 11-2

--- mountainhiking/src/tools/Acceptable.java
+++ mountainhiking/src/tools/Acceptable.java
@@ -9,7 +9,7 @@ public interface Acceptable {
     // tất cả  các regex
     public final String STU_ID_VALID = "^[CcDdHhSsQq][Ee]\\d{6}$";
     public final String NAME_VALID = "^\\D{2,20}$";
-    public final String DOUBLE_VALID = "^-?\\\\d+([.,]\\\\d+)?$";
+    public final String DOUBLE_VALID = "^-?\\d+([.,]\\d+)?$";
     public final String INTEGER_VALID = "\\d+";
     public final String PHONE_VALID = "^(03|07|08|09|01[2|6|8|9])[0-9]{8}$";
     public final String VIETTEL_VALID = "(086|096|097|098|032|033|034|035|036|037|038|039)[0-9]{7}";
